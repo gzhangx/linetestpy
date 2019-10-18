37d74fbbdd1452898ab5faf7cce46ea4dff1aa0c
Gang Zhang <gzhangx@hotmail.com>
2019-10-18 15:09:27 -0400

    test

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import numpy as np
 
 from PIL import Image
 
+import matplotlib.cbook as cbook
+import matplotlib.image as image
+import matplotlib.pyplot as plt
+
 im = Image.open('../dove.jpg') # Can be many different formats.
 #im.show()
 pix = im.load()
@@ -30,11 +34,66 @@ outpix = out.load()
 (w,h) = im.size
 
 links = np.full((int(w/2),int(h/2)),0)
+
+linksLoc = np.full((int(w/2),int(h/2),2),(0,0))
+
+RESOLUTION = 10
+
+def toLinnkPos(cur):
+    (x,y) = cur
+    fx = int(x/RESOLUTION)
+    fy = int(y/RESOLUTION)
+    return (fx, fy)
+
+def toLinkLoc(pix, pos, size):
+    (x,y) = pos
+    (w,h) = size
+    movemax=5
+    upper = y
+    lower = y
+    m = 0
+    while upper >=0:
+        if pix[x,upper] != 0:
+            break
+        m = m + 1
+        if m >= movemax:
+             break
+        upper=upper - 1
+
+    m = 0
+    while lower < h:
+        if pix[x,lower] != 0:
+            break
+        m = m + 1
+        if m >= movemax:
+         break
+        lower = lower + 1
+
+    return (x, int((upper + lower)/2))
+    
+
 start = (227,291)
+
+
 for x in range(0,w):
   for y in range(0,h): 
      if pix[x,y] ==0:
-         outpix[x,y] = 1
+         (fx, fy) = toLinnkPos((x,y))
+         if (links[fx,fy] == 0):             
+             linksLoc = toLinkLoc(pix, (x,y), (w,h))
+             links[fx,fy] = 1
+             outpix[linksLoc[0],linksLoc[1]] = 1
+             if (x >= 360 and y >= 238 and x <= 361 and y <=240):
+                     print("x=%d y=%d (%d,%d) links %d" % (x,y , fx, fy,links[fx,fy]))
+                     print("upper %d lower %d" % (linksLoc[0],linksLoc[1]))
 
 
-out.show() 
+#out.show() 
+out.save('out.png')  # 
+fig, ax = plt.subplots()
+
+ax.plot([[0,0],[1,1]], '-o', ms=1, alpha=0.7, mfc='orange')
+ax.grid()
+fig.figimage(out, 10, 10, zorder=3, alpha=.5)
+
+plt.show()
